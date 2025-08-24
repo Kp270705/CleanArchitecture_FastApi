da9e514c717e1ac616669db2e4d2a7927646feb7
Kp270705 <kunalpathak4774@gmail.com>
2025-08-24 23:50:41 +0530

    adding test and cofiguration

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from .database.core import engine, Base
-from .entities.todo import Todo
-from .entities.user import User
+from .entities.todo import Todo  # Import models to register them
+from .entities.user import User  # Import models to register them
 from .api import register_routes
+from .logging import configure_logging, LogLevels
 
 
-app = FastAPI(title="Clean Architecture FastAPI Example")
+configure_logging(LogLevels.info)
 
-Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+""" Only uncomment below to create new tables, 
+otherwise the tests will fail if not connected
+"""
+# Base.metadata.create_all(bind=engine)
+
+register_routes(app)
